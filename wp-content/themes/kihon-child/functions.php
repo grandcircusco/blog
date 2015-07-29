ad6fe283cf3ca131729283b1c6331ca47ad84bfa
Xinrui <xinruiyang1@gmail.com>
2015-07-29 14:42:42 -0400

    added/deleted header photos

--- wp-content/themes/kihon-child/functions.php
+++ wp-content/themes/kihon-child/functions.php
@@ -4,4 +4,20 @@ function theme_enqueue_styles() {
     wp_enqueue_style( 'parent-style', get_template_directory_uri() . '/style.css' );
 
 }
+
+
+function child_kihon_custom_header_setup() {
+	add_theme_support( 'custom-header', apply_filters( 'kihon_custom_header_args', array(
+		'default-image'          => '',
+		'default-text-color'     => '',
+		'width'                  => 1000,
+		'height'                 => 250,
+		'flex-height'            => true,
+		'wp-head-callback'       => 'kihon_header_style',
+		'admin-head-callback'    => '',
+		'admin-preview-callback' => 'kihon_admin_header_image',
+	) ) );
+}
+remove_action('after_setup_theme', 'kihon_custom_header_setup');
+add_action( 'after_setup_theme', 'child_kihon_custom_header_setup' );
 ?>
